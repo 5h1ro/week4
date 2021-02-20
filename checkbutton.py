5dab7431e7c03b34e5aad158c5b7796d63b742ff
5h1ro <rajamodus7073@gmail.com>
2021-02-20 17:51:46 +0700

    checkbutton

--- checkbutton.py
+++ checkbutton.py
@@ -18,122 +18,89 @@ class LoginFrame(tkinter.Frame):
     def utama(self) :
 
         self['background'] = "#ccf2f4"
-        # membuat label
-        self.Nama = tkinter.Label(self, text="Username              :")
-        self.Nama['fg'] = '#0a043c'
-        self.Nama['bg'] = '#ccf2f4'
-        self.Nama['font'] = 'Helvetica 12 bold'
-        self.Nama.grid(row=0, column=0, sticky=W)
-        # membuat entry nama
-        self.nama = Entry(self)
-        # merubah bg
-        self.nama['bg'] = '#a4ebf3'
-        # merubah ukuran border
-        self.nama['bd'] = '2px'
-        # hover
-        self.nama['cursor'] = 'spider' 
-        # merubah warna saat di select
-        self.nama['selectbackground'] = '#e40017'
-        # merubah warna text saat di select
-        self.nama['selectforeground'] = '#ff75a0'
-        # merubah width saat di select
-        self.nama['selectborderwidth'] = '2px'
-        # merubah warna fg atau text
-        self.nama['fg'] = '#0a043c' 
-        # merubah font
-        self.nama['font'] = 'Helvetica' 
-        # merubah width
-        self.nama['width'] = '30'
-        # set nama variable
-        self.nama['textvariable'] = 'idNama' 
-        # membuat grid
-        self.nama.grid(row=0, column=1, sticky=W)
-
-# password
-
-        self.Password = tkinter.Label(self, text="Password              :")
-        self.Password['fg'] = '#0a043c'
-        self.Password['bg'] = '#ccf2f4'
-        self.Password['font'] = 'Helvetica 12 bold'
-        self.Password.grid(row=1, column=0, sticky=W)
-        # membuat entry nama
-        self.password = Entry(self)
-        # merubah bg
-        self.password['bg'] = '#a4ebf3'
-        # merubah ukuran border
-        self.password['bd'] = '2px'
-        # hover
-        self.password['cursor'] = 'heart' 
-        # merubah warna fg atau text
-        self.password['fg'] = '#0a043c' 
-        # merubah font
-        self.password['font'] = 'Helvetica' 
-        # merubah tampilan inputan
-        self.password['show'] = '*'
-        # merubah width
-        self.password['width'] = '30'
-        # set nama variable
-        self.password['textvariable'] = 'idPass' 
-        # membuat grid
-        self.password.grid(row=1, column=1, sticky=W)
-        
 # Checkbutton
 
         self.cek = StringVar() 
         
-        def text():
-            self.text = Label(self)
-            self.text['text']=self.cek.get()
-            self.text.grid(row=3, column=1)
-        self.show = Checkbutton(self,
-            activebackground='#91091e',
-            activeforeground='#94ebcd'
+        self.show = Checkbutton(self, 
+        # set bg saat di click
+                activebackground='#91091e',
+        # set fg saat di click 
+                activeforeground='#94ebcd'
         )
-        # membuat grid
+        # set bg
         self.show['bg'] = '#ccf2f4'
+        # set ukuran border
         self.show['bd'] = '10'
-        # self.show['highlightcolor']
-        # self.show['disableforeground'] = 'disable'
-        self.show['font'] = 'Helvetica 12 bold'
-        self.show['fg'] = '#0a043c'
-        self.show['height'] = '1'
-        self.show['justify'] = 'left'
-        self.show['offvalue']="Off"
-        self.show['onvalue']="On"
-        self.show['padx'] = '20'
-        self.show['pady'] = '20'
-        self.show['relief'] = 'sunken'
-        self.show['selectcolor'] = 'green'
-        self.show['wraplength'] = 100
+        # merubah bentuk cursor
         self.show['cursor'] = 'heart' 
-        self.show['text'] = 'Tampilkan Password'
-        self.show['underline'] = '0'
-        self.show['variable'] = self.cek
-        self.show['width'] = '10'
-        self.show.grid(row=3, column=0, sticky=W)
+        # merubah font
+        self.show['font'] = 'Helvetica 12 bold'
+        # merubah warna fg
+        self.show['fg'] = '#0a043c'
+        # merubah tinggi
+        self.show['height'] = '1'
 
-        
+        # self.show['highlightcolor']
+
+        # merubah rata
+        self.show['justify'] = 'left'
+        # set value saat tidak di pilih
+        self.show['offvalue']="Off"
+        # set value saat di pilih
+        self.show['onvalue']="On"
+        # set padding horizontal
+        self.show['padx'] = '20'
+        # set padding vertical
+        self.show['pady'] = '20'
+        # merubah bentuk dari border
+        self.show['relief'] = 'sunken'
+        # merubah warna kotak check
+        self.show['selectcolor'] = 'blue'
+        # memberi huruf pada huruf
+        self.show['underline'] = '0'
+        # mendeklarasikan jenis variable
+        self.show['variable'] = self.cek
+        # mengatur panjang checkbutton
+        self.show['width'] = '10'
+        # mengatur panjang huruf
+        self.show['wraplength'] = 100
+        # mengatur text
+        self.show['text'] = 'Tampilkan Password'
+        self.show.grid(row=3, column=0, sticky=W)
         def text():
             self.text = Label(self)
             self.text['text']=self.cek.get()
             self.text.grid(row=3, column=1)
-
         self.show['command'] = text
 
 # gambar
         self.gambar = Checkbutton(self)
         # deklarasi foto
         self.photos = PhotoImage(file = "logo.png") 
-        #mengatur ukuran button
+        # mengatur ukuran button
         self.photoimages = self.photos.subsample(20, 20) 
+        # mengatur image pada Checkbutton
         self.gambar['image'] = self.photoimages
+        # mengatur apakah aktif atau tidak
         self.gambar['state'] = 'disable'
         self.gambar.grid(row=4, column=0, sticky=W)
-# gambar
+# bitmap
         self.gambarbt = Checkbutton(self)
+        # mengatur gambar bitmap
         self.gambarbt['bitmap'] = 'error'
+        # mengatur apakah aktif atau tidak
         self.gambarbt['state'] = 'disable'
         self.gambarbt.grid(row=4, column=1, sticky=W)
+# bitmap
+        self.gambardf = Checkbutton(self)
+        # mengatur apakah aktif atau tidak
+        self.gambardf['state'] = 'disable'
+        # mengatur text
+        self.gambardf['text'] = 'disable'
+        # mengatur warna saat checkbutton di disable
+        self.gambardf['disabledforeground'] = 'red'
+        self.gambardf.grid(row=5, column=1, sticky=W)
 
 # button
         self.namaSelect = Button(self, command=self.show.select)
